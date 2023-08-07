b5fc26a3cb5f3839a5dbc2d0c3a971d661a53dc4
JanardhanReddy <bommireddy246@gmail.com>
2023-08-07 15:02:04 +0100

    terraform

---
+++ vars/java.groovy
@@ -0,0 +1,45 @@
+def call() {
+  pipeline {
+
+    agent {
+      node { label 'workstation'}
+    }
+
+    stages {
+
+      stage('Build') {
+        steps {
+          sh 'mvn package'
+        }
+      }
+
+      stage('Unit tests') {
+        steps {
+          echo 'unit tests'
+          // sh 'mvn test'
+        }
+      }
+
+      stage('Code Analysis') {
+        steps {
+          echo 'sonar'
+        }
+      }
+
+      stage('Security Scans') {
+        steps {
+          echo 'Security Scans'
+        }
+      }
+
+      stage('Publish a Artifact') {
+        steps {
+          echo 'Publish a Artifact'
+        }
+      }
+
+
+    }
+
+  }
+}
