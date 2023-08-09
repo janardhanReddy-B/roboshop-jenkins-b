935f4a14ebef9f45f60fe9212e8b8629408dfa60
JanardhanReddy <bommireddy246@gmail.com>
2023-08-09 12:58:55 +0100

    terraform

--- vars/mainci.groovy
+++ vars/mainci.groovy
@@ -40,9 +40,9 @@ def call() {
     stage('Security Scans') {
       echo 'Security Scans'
     }
-    stage('Docker Build') {
-      sh 'docker build -t ${component} .'
-    }
+//    stage('Docker Build') {
+//      sh 'docker build -t ${component} .'
+//    }
 
 //    if(env.TAG_NAME ==~ ".*") {
 //      stage('Publish a Artifact') {
@@ -59,6 +59,7 @@ def call() {
     if(env.TAG_NAME ==~ ".*") {
       stage('Publish a Artifact') {
         sh 'docker tag ${component}:latest 637261222008.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME}'
+        sh 'docker build -t ${component} .'
         sh 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 637261222008.dkr.ecr.us-east-1.amazonaws.com'
         sh 'docker push 637261222008.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME}'
       }
