28ba7b40e0224acd01566e01196bacdd4dc9bb72
JanardhanReddy <bommireddy246@gmail.com>
2023-08-09 12:45:24 +0100

    terraform

--- vars/mainci.groovy
+++ vars/mainci.groovy
@@ -40,20 +40,30 @@ def call() {
     stage('Security Scans') {
       echo 'Security Scans'
     }
+    stage('Docker Build') {
+      sh 'docker build -t ${component} .'
+    }
 
-
+//    if(env.TAG_NAME ==~ ".*") {
+//      stage('Publish a Artifact') {
+//        if (env.cibuild == "java") {
+//            sh 'mv target/${component}-1.0.jar ${component}.jar'
+//            sh 'rm -rf pom.xml src target'
+//          }
+//        sh 'rm -f Jenkinsfile'
+//        sh 'echo ${TAG_NAME} >VERSION'
+//        sh 'zip -r ${component}-${TAG_NAME}.zip *'
+//        sh 'curl -v -u admin:admin123 --upload-file ${component}-${TAG_NAME}.zip http://172.31.19.157:8081/repository/${component}/${component}-${TAG_NAME}.zip'
+//      }
+//    }
     if(env.TAG_NAME ==~ ".*") {
       stage('Publish a Artifact') {
-        if (env.cibuild == "java") {
-            sh 'mv target/${component}-1.0.jar ${component}.jar'
-            sh 'rm -rf pom.xml src target'
-          }
-        sh 'rm -f Jenkinsfile'
-        sh 'echo ${TAG_NAME} >VERSION'
-        sh 'zip -r ${component}-${TAG_NAME}.zip *'
-        sh 'curl -v -u admin:admin123 --upload-file ${component}-${TAG_NAME}.zip http://172.31.19.157:8081/repository/${component}/${component}-${TAG_NAME}.zip'
+        sh 'docker tag ${component}:latest 637261222008.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME}'
+        sh 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 637261222008.dkr.ecr.us-east-1.amazonaws.com'
+        sh 'docker push 637261222008.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME}'
       }
     }
+
 
   }
 }
