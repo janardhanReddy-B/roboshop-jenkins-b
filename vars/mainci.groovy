eb99b04ea2e36cd29ce00c4f4fd443a2b6a34ac3
JanardhanReddy <bommireddy246@gmail.com>
2023-08-10 12:49:25 +0100

    terraform

--- vars/mainci.groovy
+++ vars/mainci.groovy
@@ -58,8 +58,8 @@ def call() {
 //    }
     if(env.TAG_NAME ==~ ".*") {
       stage('Publish a Artifact') {
-        sh 'docker tag ${component}:latest 637261222008.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME}'
         sh 'docker build -t ${component} .'
+        sh 'docker tag ${component}:latest 637261222008.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME}'
         sh 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 637261222008.dkr.ecr.us-east-1.amazonaws.com'
         sh 'docker push 637261222008.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME}'
       }
